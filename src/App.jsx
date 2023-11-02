9e9c37847a8bdeda294f9bc07da8044fe96c6178
Pablo Ribeiro <pabloribeiro2302@gmail.com>
2023-11-01 22:14:03 -0300

    commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Info from "./components/Info"
+import Main from "./components/Main"
+import Footer from "./components/Footer"
+
+function App() {
+
+  return (
+    <>
+      <Info />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+function Main() {
+    return(
+        <div className="mainText-container">
+            <h2 className="mainText-subtitle">About</h2>
+            <p className="mainText-paragraph">I'm a passionate student of Web Development, and a self-taught apprentice.
+                My main focus is about Frontend Development and creating responsive sites.
+            </p>
+            <p className="mainText-paragraph">
+                I've got solid skills in HTML, CSS, JavaScript, and React that allow me to create efficient and impactfull user experiences. 
+            </p>
+            <h2 className="mainText-subtitle">Interests</h2>
+            <p className="mainText-paragraph">Martial arts practiceneer. Muay Thai teacher. Gamer. Coffee Fanatic. Guitar Player</p>
+        </div>
+    ) 
+}
+
+export default Main;
